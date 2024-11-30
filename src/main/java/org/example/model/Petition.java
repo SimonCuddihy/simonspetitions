f578243673e221e61ac5c7c118c507a879be5f6a
Simon Cuddihy <simoncuddihy@gmail.com>
2024-11-30 16:46:47 +0000

    Initial Petition model with basic fields

---
+++ src/main/java/org/example/model/Petition.java
@@ -0,0 +1,23 @@
+package org.example.model;
+
+public class Petition {
+    private Long id;
+    private String title;
+
+    // Getters/setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
